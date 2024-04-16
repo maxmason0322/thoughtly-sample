0f36510d132d268bcd177be6d1e2b9422f396abe
max <maxdoylemason@gmail.com>
2024-04-16 14:20:08 -0600

    icon component

--- src/components/Buttons/Primary.tsx
+++ src/components/Buttons/Primary.tsx
@@ -1,18 +1,4 @@
-import { ReactComponent as AnalyticsSVG } from "images/global/icons/Analytics.svg"
-import { ReactComponent as ArrowOneSVG } from "images/global/icons/Arrow-one.svg"
-import { ReactComponent as CalendarSVG } from "images/global/icons/Calendar.svg"
-import { ReactComponent as ChevSVG } from "images/global/icons/Chev.svg"
-import { ReactComponent as ChevDownSVG } from "images/global/icons/ChevDown.svg"
-import { ReactComponent as CopyPasteSVG } from "images/global/icons/CopyPaste.svg"
-import { ReactComponent as GeniusSVG } from "images/global/icons/Genius.svg"
-import { ReactComponent as IntegrationSVG } from "images/global/icons/Integration.svg"
-import { ReactComponent as LockSVG } from "images/global/icons/Lock.svg"
-import { ReactComponent as NodesSVG } from "images/global/icons/Nodes.svg"
-import { ReactComponent as PhoneSVG } from "images/global/icons/Phone.svg"
-import { ReactComponent as RoutingSVG } from "images/global/icons/Routing.svg"
-import { ReactComponent as TargetSVG } from "images/global/icons/Target.svg"
-import { ReactComponent as TrashSVG } from "images/global/icons/Trash.svg"
-import { ReactComponent as VerifiedSVG } from "images/global/icons/Verified.svg"
+import Icon, { type IconType } from "components/Icon"
 import UniversalLink, {
 	type UniversalLinkProps,
 } from "library/Loader/UniversalLink"
@@ -21,28 +7,10 @@ import styled, { css } from "styled-components"
 import colors from "styles/colors"
 import textStyles from "styles/text"
 
-const iconMap = {
-	calendar: CalendarSVG,
-	chev: ChevSVG,
-	trash: TrashSVG,
-	nodes: NodesSVG,
-	lock: LockSVG,
-	target: TargetSVG,
-	analytics: AnalyticsSVG,
-	arrowOne: ArrowOneSVG,
-	chevDown: ChevDownSVG,
-	copyPaste: CopyPasteSVG,
-	genius: GeniusSVG,
-	integration: IntegrationSVG,
-	phone: PhoneSVG,
-	routing: RoutingSVG,
-	verified: VerifiedSVG,
-}
-
 type Props = UniversalLinkProps & {
 	outline?: boolean
 	variant?: "primary" | "secondary"
-	icon?: keyof typeof iconMap
+	icon?: IconType
 }
 
 export default function Primary({
@@ -52,8 +20,6 @@ export default function Primary({
 	children,
 	...props
 }: Props) {
-	const Icon = icon ? iconMap[icon] : null
-
 	return (
 		<Wrapper {...props}>
 			{outline && <Border />}
@@ -61,7 +27,7 @@ export default function Primary({
 				{variant !== "secondary" && <Highlight />}
 				<Span>
 					{children}
-					{Icon && <Icon />}
+					{icon && <Icon name={icon} />}
 				</Span>
 			</Inner>
 		</Wrapper>
---
+++ src/components/Icon.tsx
@@ -0,0 +1,40 @@
+import { ReactComponent as AnalyticsSVG } from "images/global/icons/Analytics.svg"
+import { ReactComponent as ArrowOneSVG } from "images/global/icons/Arrow-one.svg"
+import { ReactComponent as CalendarSVG } from "images/global/icons/Calendar.svg"
+import { ReactComponent as ChevSVG } from "images/global/icons/Chev.svg"
+import { ReactComponent as ChevDownSVG } from "images/global/icons/ChevDown.svg"
+import { ReactComponent as CopyPasteSVG } from "images/global/icons/CopyPaste.svg"
+import { ReactComponent as GeniusSVG } from "images/global/icons/Genius.svg"
+import { ReactComponent as IntegrationSVG } from "images/global/icons/Integration.svg"
+import { ReactComponent as LockSVG } from "images/global/icons/Lock.svg"
+import { ReactComponent as NodesSVG } from "images/global/icons/Nodes.svg"
+import { ReactComponent as PhoneSVG } from "images/global/icons/Phone.svg"
+import { ReactComponent as RoutingSVG } from "images/global/icons/Routing.svg"
+import { ReactComponent as TargetSVG } from "images/global/icons/Target.svg"
+import { ReactComponent as TrashSVG } from "images/global/icons/Trash.svg"
+import { ReactComponent as VerifiedSVG } from "images/global/icons/Verified.svg"
+
+const iconMap = {
+	calendar: CalendarSVG,
+	chev: ChevSVG,
+	trash: TrashSVG,
+	nodes: NodesSVG,
+	lock: LockSVG,
+	target: TargetSVG,
+	analytics: AnalyticsSVG,
+	arrowOne: ArrowOneSVG,
+	chevDown: ChevDownSVG,
+	copyPaste: CopyPasteSVG,
+	genius: GeniusSVG,
+	integration: IntegrationSVG,
+	phone: PhoneSVG,
+	routing: RoutingSVG,
+	verified: VerifiedSVG,
+}
+
+export type IconType = keyof typeof iconMap
+
+export default function Icon({ name }: { name: IconType }) {
+	const Svg = iconMap[name]
+	return <Svg />
+}
