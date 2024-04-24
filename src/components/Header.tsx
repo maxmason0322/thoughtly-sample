bce8ba7bed5682fe228abeae4686c6605a48d581
Dallen Hoyal <dallen.hoyal@gmail.com>
2024-04-24 16:27:29 -0600

    mobile

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,15 +2,23 @@ import PrimaryButton from "components/Buttons/Primary"
 import gsap from "gsap"
 import ScrollToPlugin from "gsap/ScrollToPlugin"
 import { ReactComponent as LogoSVG } from "images/global/logo.svg"
-import { fresponsive, ftablet } from "library/fullyResponsive"
+import UniversalLink from "library/Loader/UniversalLink"
+import { ScreenContext } from "library/ScreenContext"
+import { fmobile, fresponsive, ftablet } from "library/fullyResponsive"
+import { useContext } from "react"
 import styled, { css } from "styled-components"
+import colors from "styles/colors"
 import { desktopBreakpoint } from "styles/media"
+import textStyles from "styles/text"
 import links from "utils/links"
 import Link from "./Buttons/Link"
+import Icon from "./Icon"
 
 gsap.registerPlugin(ScrollToPlugin)
 
 export default function Header() {
+	const { mobile } = useContext(ScreenContext)
+
 	const scrollTo = (section: string, offset: number) => {
 		gsap.to(window, {
 			scrollTo: {
@@ -42,12 +50,24 @@ export default function Header() {
 					</Links>
 				</Left>
 				<Right>
-					<PrimaryButton variant="secondary" to={links.todo}>
-						Sign In
-					</PrimaryButton>
-					<PrimaryButton icon="chev" to={links.todo}>
-						Get Started
-					</PrimaryButton>
+					{mobile && (
+						<>
+							<MobileButton to={links.todo}>Sign In</MobileButton>
+							<MobileButton to={links.todo}>
+								Get Started <StyledIcon name="chev" />
+							</MobileButton>
+						</>
+					)}
+					{!mobile && (
+						<>
+							<PrimaryButton variant="secondary" to={links.todo}>
+								Sign In
+							</PrimaryButton>
+							<PrimaryButton icon="chev" to={links.todo}>
+								Get Started
+							</PrimaryButton>
+						</>
+					)}
 				</Right>
 			</Inner>
 		</Wrapper>
@@ -77,6 +97,10 @@ const Inner = styled.div`
   ${ftablet(css`
     padding: 48px 68px 0;
   `)}
+
+  ${fmobile(css`
+    padding: 24px 28.5px 0;
+  `)}
 `
 
 const StyledLogoSVG = styled(LogoSVG)`
@@ -84,6 +108,10 @@ const StyledLogoSVG = styled(LogoSVG)`
 
   ${fresponsive(css`
     width: 138px;
+  `)}
+
+  ${fmobile(css`
+    width: 103.5px;
   `)}
 `
 
@@ -104,6 +132,10 @@ const Right = styled.div`
   ${fresponsive(css`
     gap: 16px;
   `)}
+
+  ${fmobile(css`
+    gap: 11px;
+  `)}
 `
 
 const Links = styled.div`
@@ -116,5 +148,28 @@ const Links = styled.div`
 
   ${ftablet(css`
     display: none;
+  `)}
+
+  ${fmobile(css`
+    display: none;
+  `)}
+`
+
+const MobileButton = styled(UniversalLink)`
+  ${textStyles.sh4}
+  color: ${colors.black};
+  display: flex;
+  align-items: center;
+
+  ${fresponsive(css`
+    padding: 8px 10px;
+    gap: 2px;
+  `)}
+`
+
+const StyledIcon = styled(Icon)`
+  ${fmobile(css`
+    width: 12px;
+    height: 12px;
   `)}
 `
