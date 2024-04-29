b9b043413f48438ae8879d5d4f880c9126b96998
max <maxdoylemason@gmail.com>
2024-04-29 09:52:50 -0600

    Privacy policy and T&C details

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import Seo from "components/Seo"
-import FinalCTA from "sections/09-FinalCTA"
 import Hero from "sections/home/01-Hero"
 import Industry from "sections/home/02-Industry"
 import Statement from "sections/home/03-Statement"
@@ -7,6 +6,7 @@ import Features from "sections/home/04-Features"
 import CallCTA from "sections/home/05-CallCTA"
 import Integrations from "sections/home/075-Integrations.tsx"
 import WidgetsAndVideo from "sections/home/08-WidgetsAndVideo"
+import FinalCTA from "sections/home/09-FinalCTA"
 import Pricing from "sections/home/Pricing"
 import SocialProof from "sections/home/SocialProof"
 
