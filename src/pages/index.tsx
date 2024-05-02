2cec8737dc81cb6a2265a015796c319d5f903420
Robbie <robbiethewadley@gmail.com>
2024-05-02 11:41:16 -0600

    maybe reduce initial dom size?

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Seo from "components/Seo"
+import ClientOnly from "library/ClientOnly"
 import { Suspense, lazy } from "react"
 
 import Hero from "sections/home/01-Hero"
@@ -24,27 +25,29 @@ export default function IndexPage() {
 			<Suspense fallback={<div>Loading...</div>}>
 				<Statement />
 			</Suspense>
-			<Suspense fallback={<div>Loading...</div>}>
-				<Features />
-			</Suspense>
-			<Suspense fallback={<div>Loading...</div>}>
-				<CallCTA />
-			</Suspense>
-			<Suspense fallback={<div>Loading...</div>}>
-				<WidgetsAndVideo />
-			</Suspense>
-			<Suspense fallback={<div>Loading...</div>}>
-				<Workflows />
-			</Suspense>
-			<Suspense fallback={<div>Loading...</div>}>
-				<Integrations />
-			</Suspense>
-			<Suspense fallback={<div>Loading...</div>}>
-				<Pricing />
-			</Suspense>
-			<Suspense fallback={<div>Loading...</div>}>
-				<FinalCTA />
-			</Suspense>
+			<ClientOnly>
+				<Suspense fallback={<div>Loading...</div>}>
+					<Features />
+				</Suspense>
+				<Suspense fallback={<div>Loading...</div>}>
+					<CallCTA />
+				</Suspense>
+				<Suspense fallback={<div>Loading...</div>}>
+					<WidgetsAndVideo />
+				</Suspense>
+				<Suspense fallback={<div>Loading...</div>}>
+					<Workflows />
+				</Suspense>
+				<Suspense fallback={<div>Loading...</div>}>
+					<Integrations />
+				</Suspense>
+				<Suspense fallback={<div>Loading...</div>}>
+					<Pricing />
+				</Suspense>
+				<Suspense fallback={<div>Loading...</div>}>
+					<FinalCTA />
+				</Suspense>
+			</ClientOnly>
 		</>
 	)
 }
