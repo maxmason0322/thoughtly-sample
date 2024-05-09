7d56835ba03305629d6c84422ad826ef303334cc
davidthecomposer <davidthecomposer@gmail.com>
2024-05-09 11:57:51 -0600

    sctript added

--- src/components/Seo.tsx
+++ src/components/Seo.tsx
@@ -100,6 +100,11 @@ export default function Seo({
 			<meta property="og:type" content="website" />
 
 			<Script>{`!function () {var reb2b = window.reb2b = window.reb2b || [];if (reb2b.invoked) return;reb2b.invoked = true;reb2b.methods = ["identify", "collect"];reb2b.factory = function (method) {return function () {var args = Array.prototype.slice.call(arguments);args.unshift(method);reb2b.push(args);return reb2b;};};for (var i = 0; i < reb2b.methods.length; i++) {var key = reb2b.methods[i];reb2b[key] = reb2b.factory(key);}reb2b.load = function (key) {var script = document.createElement("script");script.type = "text/javascript";script.async = true;script.src = "https://s3-us-west-2.amazonaws.com/b2bjsstore/b/" + key + "/reb2b.js.gz";var first = document.getElementsByTagName("script")[0];first.parentNode.insertBefore(script, first);};reb2b.SNIPPET_VERSION = "1.0.1";reb2b.load("ZQOQRJHP5Y62");}();`}</Script>
+
+			<Script
+				src="https://cdn.tolt.io/tolt.js"
+				data-tolt="fa4de1bb-0c6b-4c2c-b4a6-2070c944c39b"
+			/>
 		</>
 	)
 }
