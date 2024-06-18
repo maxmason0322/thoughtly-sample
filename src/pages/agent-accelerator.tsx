55f8f458f4f3f8586ff49384c407b2198d36eaa0
Robbie <robbiethewadley@gmail.com>
2024-06-18 10:00:23 -0600

    set up preloader animation

--- src/pages/agent-accelerator.tsx
+++ src/pages/agent-accelerator.tsx
@@ -1,3 +1,7 @@
+import { BEAT_ONE_DURATION, BEAT_TWO_DURATION } from "components/Preloader"
+import gsap from "gsap"
+import { usePreloader } from "library/Loader/PreloaderUtils"
+import { useRef } from "react"
 import AgentsHero from "sections/agents/01-Hero"
 import AgentsOverview from "sections/agents/02-Overview"
 import AgentsHowItWorks from "sections/agents/03-HowItWorks"
@@ -5,10 +9,29 @@ import AgentsAdvantages from "sections/agents/04-Advantages"
 import CallToAction from "sections/agents/06-CallToAction"
 
 export default function AgentAccelerator() {
+	const wrapperRef = useRef<HTMLDivElement | null>(null)
+
+	usePreloader({
+		only: "whenAtTop",
+		duration: 2,
+		callback: () => {
+			const duration = BEAT_TWO_DURATION
+			const delay = BEAT_ONE_DURATION
+			gsap.from(wrapperRef.current, {
+				y: "100lvh",
+				duration,
+				delay,
+				ease: "power3.inOut",
+			})
+		},
+	})
+
 	return (
 		<>
-			<AgentsHero />
-			<AgentsOverview />
+			<div ref={wrapperRef}>
+				<AgentsHero />
+				<AgentsOverview />
+			</div>
 			<AgentsHowItWorks />
 			<AgentsAdvantages />
 			<CallToAction />
