7a78295b0a9fd103cd619c16d94f103c007421b5
max <maxdoylemason@gmail.com>
2024-06-24 17:17:41 -0600

    fix Seo syntax

--- src/pages/platform.tsx
+++ src/pages/platform.tsx
@@ -1,14 +1,14 @@
 import Seo from "components/Seo"
 import Hero from "sections/platform/01-Hero"
 import PlatformAdvantage from "sections/platform/04-Advantage"
-        
+
 export default function Platform() {
 	return (
-    <>
-      <Hero />
-      <PlatformAdvantage />
-    </>
-  )
+		<>
+			<Hero />
+			<PlatformAdvantage />
+		</>
+	)
 }
 
 export function Head() {
@@ -18,4 +18,5 @@ export function Head() {
 			description="The AI Agent Platform that does it all."
 			pathname="/platform"
 		/>
+	)
 }
