801c57d2e08b0148ec3acce262af38a3b17ddbac
davidthecomposer <davidthecomposer@gmail.com>
2024-06-26 12:34:24 -0600

    hydration cnosole

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -65,6 +65,7 @@ export const replaceHydrateFunction = process.env.NETLIFY // don't replace in de
 			return (element: ReactNode, rootElement: Element) => {
 				try {
 					startTransition(() => {
+						console.log("starting hydrateRoot")
 						hydrateRoot(rootElement, element, {
 							onRecoverableError: console.error,
 						})
