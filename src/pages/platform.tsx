14717c0e7d323042771556cdd82d49f9e7ea718f
max <maxdoylemason@gmail.com>
2024-07-02 14:29:23 -0600

    Merge pull request #121 from reformcollective/platform-hero
    Platform hero

--- src/pages/platform.tsx
+++ src/pages/platform.tsx
@@ -1,9 +1,22 @@
+import Seo from "components/Seo"
+import Hero from "sections/platform/01-Hero"
 import PlatformAdvantage from "sections/platform/04-Advantage"
 
 export default function Platform() {
 	return (
 		<>
+			<Hero />
 			<PlatformAdvantage />
 		</>
+	)
+}
+
+export function Head() {
+	return (
+		<Seo
+			title="Thoughtly | Platform"
+			description="The AI Agent Platform that does it all."
+			pathname="/platform"
+		/>
 	)
 }
